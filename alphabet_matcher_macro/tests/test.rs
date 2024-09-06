abe72987138f920e365e61ad900b71f96f984dcf
RoDmitry <gh@rdmtr.com>
2024-09-06 05:07:28 +0000

    alphabet_matcher_macro

---
+++ alphabet_matcher_macro/tests/test.rs
@@ -0,0 +1,24 @@
+use alphabet_matcher_macro::*;
+
+#[derive(Debug, PartialEq)]
+enum SomeEnum {
+    A,
+    B,
+}
+
+#[test]
+fn test() {
+    let ch = 'a';
+
+    let res: &[SomeEnum] =
+        alphabet_match!([(SomeEnum::A, ['a', 'b', 'c']), (SomeEnum::B, ['a', 'c']),]);
+
+    assert_eq!(res, &[SomeEnum::A, SomeEnum::B]);
+
+    let ch = 'd';
+
+    let res: &[SomeEnum] =
+        alphabet_match!([(SomeEnum::A, ['a', 'b', 'c']), (SomeEnum::B, ['a', 'c']),]);
+
+    assert_eq!(res, &[SomeEnum::A, SomeEnum::B]);
+}
