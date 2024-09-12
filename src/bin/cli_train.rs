141681d9e664ef21a92154e66c4cd82bd9c37cfd
RoDmitry <gh@rdmtr.com>
2024-09-12 15:12:32 +0000

    Cli train fix

--- src/bin/cli_train.rs
+++ src/bin/cli_train.rs
@@ -96,18 +96,17 @@ fn main() {
     })
     .collect(); */
 
-    /* let mut buf: Vec<u8> = Vec::new();
+    let mut buf: Vec<u8> = Vec::new();
     io::stdin()
         .read_to_end(&mut buf)
-        .expect("expected input via stdin"); */
-    // let text = String::from_utf8(buf).expect("Input should be valid utf-8");
+        .expect("expected input via stdin");
+    let text = String::from_utf8(buf).expect("Input should be valid utf-8");
+    // let lines = io::stdin().lines().map(|r| r.unwrap()).filter(|line| !line.trim().is_empty());
 
     let output_directory_path = Path::new(&args.out);
-    let lines = io::stdin().lines().map(|r| r.unwrap()).filter(|line| !line.trim().is_empty());
-
     let result = LanguageModelFilesWriter::create_and_write_language_model(
         output_directory_path,
-        lines,
+        text,
         &Language::Japanese,
     );
     println!("{:?}", result)
